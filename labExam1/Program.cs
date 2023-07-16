a3067c0d2f033e5ee51ca351fca49916e03ddafd
Rutik Yerawar <rutikyerawar2605@gmail.com>
2023-07-16 13:20:21 +0530

    Add project files.

---
+++ labExam1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+namespace labExam1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] daysOfWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            var days = from day in daysOfWeek select day;
+
+            Console.WriteLine("Days of the week:");
+
+            foreach (var day in days)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
